d9dd982f22fbba4f9df46d99cc8d2a62cbe5a960
Coutj <coutinholima.j@gmail.com>
2021-06-02 22:33:20 -0300

    Trabalho terminado e comentado

--- trabalho_2_Juan_Lima.py
+++ trabalho_2_Juan_Lima.py
@@ -2,7 +2,7 @@ import pandas as pd
 import math
 import numpy as np
 from scipy.stats import pearsonr
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures, LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, SGDRegressor, LogisticRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -10,11 +10,12 @@ from sklearn.linear_model import Ridge, Lasso
 from sklearn.ensemble import RandomForestRegressor
 from matplotlib import pyplot as plt
 
-
+# importando conjunto de treino
 conjunto_treino_df = pd.read_csv(
     "eel891-202002-trabalho-2/conjunto_de_treinamento.csv"
 )
 
+# listando variaveis cartegoricas
 variaveis_categoricas = [
     coluna for coluna in conjunto_treino_df.columns
     if conjunto_treino_df[coluna].dtype == 'object'
@@ -22,8 +23,6 @@ variaveis_categoricas = [
 
 print("\nVariaveis categoricas\n")
 print(variaveis_categoricas)
-
-# ['tipo', 'bairro', 'tipo_vendedor', 'diferenciais']
 
 print('\nVerificar a quantidade de amostras em diferenciais:\n')
 print(conjunto_treino_df['diferenciais'].value_counts())
@@ -46,7 +45,7 @@ for var_categorica in variaveis_categoricas:
     )
 
 
-########### Dividindo em regioes politico administrativas de recife ######
+# ########## Dividindo em regioes politico administrativas de recife ######
 
 dict_regioes = {
     'RPA1': [
@@ -181,10 +180,30 @@ conjunto_treino_df = pd.get_dummies(
     columns=['RPA'],
     prefix='RPA',
 )
-###################################################################
+# ##################################################################
 
-####### Coeficiente de pearson em todas as colunas ################
-remover = ['Id', 'tipo', 'bairro', 'tipo_vendedor', 'diferenciais']
+# ############ One-hot encoding tipo de habitacao ##################
+
+conjunto_treino_df = pd.get_dummies(
+    conjunto_treino_df,
+    columns=['tipo'],
+    prefix='tipo',
+)
+
+# ##################################################################
+
+# ###########  Binarizando tipo de vendedor ########################
+
+binarizador = LabelBinarizer()
+conjunto_treino_df['tipo_vendedor'] = binarizador.fit_transform(
+        conjunto_treino_df['tipo_vendedor']
+    )
+
+# ##################################################################
+
+# ###### Coeficiente de pearson em todas as colunas ################
+
+remover = ['Id', 'bairro', 'diferenciais']
 conjunto_treino_calculo_pearson = conjunto_treino_df.drop(remover, axis=1)
 
 pearson_dict = dict()
@@ -206,11 +225,9 @@ pearson_dict_sorted = sorted(
 pearson_dict_sorted = dict(pearson_dict_sorted)
 
 for item in pearson_dict_sorted:
-    print('{:<15} = {:.4f}'.format(item, pearson_dict[item]))
+    print('{:<17} = {:.4f}'.format(item, pearson_dict[item]))
 
 print()
-
-conjunto_treino_df.plot.scatter(x='preco', y='suites')
 #######################################################
 
 colunas = conjunto_treino_df.columns
@@ -220,28 +237,31 @@ print(colunas)
 print("\nDescartando variaveis desnecessarias\n")
 remover = [
             'Id',
-            'tipo',
+            # 'tipo_Casa',
+            'tipo_Apartamento',
+            'tipo_Loft',
+            'tipo_Quitinete',
             'bairro',
-            'tipo_vendedor',
+            # 'tipo_vendedor',
             # 'quartos',
             # 'suites',
-            'vagas',
+            # 'vagas',
             # 'area_util',
-            # 'area_extra',
+            'area_extra',
             'diferenciais',
             'churrasqueira',
             # 'estacionamento',
             'piscina',
-            'playground',
+            # 'playground',
             'quadra',
             's_festas',
             's_jogos',
-            's_ginastica',
-            'sauna',
+            # 's_ginastica',
+            # 'sauna',
             # 'vista_mar',
             # 'preco',
             'RPA_RPA1',
-            # 'RPA_RPA2',
+            'RPA_RPA2',
             # 'RPA_RPA3',
             'RPA_RPA4',
             # 'RPA_RPA5',
@@ -251,6 +271,7 @@ remover = [
 conjunto_treino_df = conjunto_treino_df.drop(remover, axis=1)
 print(conjunto_treino_df)
 
+# Separando dados de treino e dados alvo
 dados_treino = conjunto_treino_df.iloc[:, conjunto_treino_df.columns != 'preco'].to_numpy()
 dados_alvo = conjunto_treino_df.iloc[:, conjunto_treino_df.columns == 'preco'].to_numpy()
 dados_alvo = dados_alvo.ravel()
@@ -263,42 +284,51 @@ x_treino, x_teste, y_treino, y_teste = train_test_split(
     random_state=1543
 )
 
+# aplicando escala nos conjuntos de treino e teste
 escalador = StandardScaler()
 escalador.fit(x_treino)
 x_treino = escalador.transform(x_treino)
 x_teste = escalador.transform(x_teste)
 
-# scale.fit(x_treino)
-# x_treino = scale.transform(x_treino)
-# x_teste = scale.transform(x_teste)
+
+
+# ################## Iteracao para comparacao de modelos ##########
+# Os regressores estao divididos em blocos de maneira que para testar 
+# basta comentar e descomentar os blocos
 
 print('K  Resultado')
 print('--- ---------------')
 
-for k in range(1, 2):
+for k in range(-5, 4):
+
 
     # pf = PolynomialFeatures(degree=k)
     # pf = pf.fit(x_treino)
     # x_treino_poly = pf.transform(x_treino)
     # x_teste_poly = pf.transform(x_teste)
 
+
     # regressor_linear = LinearRegression()
     # regressor_linear = regressor_linear.fit(x_treino_poly, y_treino)
     # y_resposta_teste = regressor_linear.predict(x_teste_poly)
+
 
     # regressor_ridge = Ridge(alpha=1000, solver='lsqr', random_state=0)
     # regressor_ridge = regressor_ridge.fit(x_treino_poly, y_treino)
     # y_resposta_teste = regressor_ridge.predict(x_teste_poly)
 
+
     # RF_regressor = RandomForestRegressor(n_estimators=k, random_state=0)
     # RF_regressor.fit(x_treino, y_treino)
     # y_resposta_teste = RF_regressor.predict(x_teste)
 
+
     logreg = LogisticRegression(
-        # penalty='l1',
+        penalty='l2',
         # random_state=0,
         max_iter=500,
-        solver='liblinear'
+        solver='liblinear',
+        C=pow(10, k)
     )
     y_treino = y_treino.astype('int')
     logreg = logreg.fit(x_treino, y_treino)
@@ -306,7 +336,7 @@ for k in range(1, 2):
 
     # regressor_sgd = SGDRegressor(
     #     loss='squared_loss',
-    #     alpha=0.6,
+    #     alpha=5,
     #     penalty='l2',
     #     # tol=1e-2,
     #     max_iter=100000,
@@ -328,16 +358,17 @@ for k in range(1, 2):
     print(' %3d %15.4f' % (k, rmspe))
 
 print()
-# Resposta
+# #############################################################################
 
-
+# Importando conjunto de teste
 conjunto_teste_df = pd.read_csv(
     "eel891-202002-trabalho-2/conjunto_de_teste.csv"
 )
 
+# Separando coluna de ID
 Ids_teste = conjunto_teste_df['Id']
 
-# Dividindo em regioes politico administrativas de recife
+# ### Dividindo em regioes politico administrativas de recife #####
 conjunto_teste_df['RPA'] = (
     conjunto_teste_df['bairro'].map(dict_estados_regioes)
 )
@@ -347,33 +378,44 @@ conjunto_teste_df = pd.get_dummies(
     columns=['RPA'],
     prefix='RPA'
 )
+####################################################################
 
+# ############ One-hot encoding tipo de habitacao ##################
+
+conjunto_teste_df = pd.get_dummies(
+    conjunto_teste_df,
+    columns=['tipo'],
+    prefix='tipo',
+)
+
+# ##################################################################
+
+# ###########  Binarizando tipo de vendedor ########################
+
+binarizador = LabelBinarizer()
+conjunto_teste_df['tipo_vendedor'] = binarizador.fit_transform(
+        conjunto_teste_df['tipo_vendedor']
+    )
+
+# ##################################################################
+
+# O conjunto de amostra nao possui categoria Quitinete
+remover.pop(remover.index('tipo_Quitinete'))
 conjunto_teste_df = conjunto_teste_df.drop(remover, axis=1)
 
+# obtendo conjunto de teste
 dados_teste = conjunto_teste_df.iloc[:, :].to_numpy()
 
+# aplicando mesma escala aplicada no conjunto de treino
 escalador = StandardScaler()
 escalador.fit(dados_treino)
 x_treino = escalador.transform(dados_treino)
 x_teste = escalador.transform(dados_teste)
 
-
-# pf = PolynomialFeatures(degree=2)
-
-# pf = pf.fit(x_treino)
-# x_treino_poly = pf.transform(x_treino)
-# x_teste_poly = pf.transform(x_teste)
-
-# regressor_linear = LinearRegression()
-# regressor_linear = regressor_linear.fit(x_treino_poly, dados_alvo)
-
-# regressor_ridge = Ridge(alpha=3200, solver='lsqr')
-# regressor_ridge = regressor_ridge.fit(x_treino_poly, dados_alvo)
-
-
+# Aplicando melhor modelo de regressao
 dados_alvo = dados_alvo.astype('int')
 logreg = LogisticRegression(
-        # penalty='l1',
+        # penalty='l2',
         # random_state=0,
         max_iter=500,
         solver='liblinear'
@@ -381,22 +423,12 @@ logreg = LogisticRegression(
 logreg.fit(x_treino, dados_alvo)
 y_resposta_teste = logreg.predict(x_teste)
 
-# regressor_sgd = SGDRegressor(
-#         loss='squared_loss',
-#         alpha=0.6,
-#         penalty='l2',
-#         # tol=1e-2,
-#         max_iter=100000,
-#         random_state=0
-#     )
-
-# regressor_sgd = regressor_sgd.fit(x_treino, dados_alvo)
-# y_resposta_teste = regressor_sgd.predict(x_teste)
-
 # compondo dataframes
 resposta = pd.DataFrame(y_resposta_teste, columns=['preco'])
 resposta = pd.concat([Ids_teste, resposta], axis=1, join='inner')
 
+print("Resultado:\n")
 print(resposta)
 
+#Salvando arquivo de respostas
 resposta.to_csv('./resposta.csv', index=False)
